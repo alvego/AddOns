e7e601d9c701d67aa99be8f5a94c9cdd7fd3f61f
Danik <kluory@gmail.com>
2013-01-26 15:13:26 +0200

    try2

---
+++ sova/binds.lua
@@ -0,0 +1,98 @@
+﻿-- Sova Rotation Helper by Timofeev Alexey & Co
+------------------------------------------------------------------------------------------------------------------
+-- Binding
+BINDING_HEADER_PRH = "Sova Rotation Helper"
+BINDING_NAME_PRH_AOE = "Вкл/Выкл AOE в ротации"
+BINDING_NAME_PRH_INTERRUPT = "Вкл/Выкл сбивание кастов"
+BINDING_NAME_PRH_AUTOAGGRO = "Авто АГГРО"
+------------------------------------------------------------------------------------------------------------------
+if CanInterrupt == nil then CanInterrupt = true end
+
+function UseInterrupt()
+    CanInterrupt = not CanInterrupt
+    if CanInterrupt then
+        echo("Interrupt: ON",true)
+    else
+        echo("Interrupt: OFF",true)
+    end 
+end
+
+------------------------------------------------------------------------------------------------------------------
+if AutoAGGRO == nil then AutoAGGRO = true end
+
+function AutoAGGROToggle()
+    AutoAGGRO = not AutoAGGRO
+    if AutoAGGRO then
+        echo("АвтоАГГРО: ON",true)
+    else
+        echo("АвтоАГГРО: OFF",true)
+    end 
+end
+
+------------------------------------------------------------------------------------------------------------------
+function IsAOE()
+    if IsShiftKeyDown() == 1 then return true end
+    return IsValidTarget("target") and InMelee("target") and TryEach(TARGETS, function(t)
+        return IsValidTarget(t) and InMelee(t) and not IsOneUnit("target", t)
+    end)
+end
+
+------------------------------------------------------------------------------------------------------------------
+-- dispel
+if DispelBlacklist == nil then DispelBlacklist = {} end
+if DispelWhitelist == nil then DispelWhitelist = {} end
+local dispelSpell = "Очищение"
+local dispelTypes = {"Poison", "Disease", "Magic"}
+function TryDispel(unit)
+    if not IsReadySpell(dispelSpell) or InGCD() or not CanHeal(unit) or HasDebuff("Нестабильное колдовство", 0.1, unit) then return false end
+    local ret = false
+    for i = 1, 40 do
+        if not ret then
+            local name, _, _, _, debuffType, duration, expirationTime   = UnitDebuff(unit, i,true) 
+            if name and (expirationTime - GetTime() >= 3 or expirationTime == 0) 
+                and (tContains(DispelWhitelist, name) or tContains(dispelTypes, debuffType) and not tContains(DispelBlacklist, name)) then
+                if DoSpell(dispelSpell, unit) then 
+                    ret = true 
+                end
+            end
+        end
+    end
+    return ret
+end
+
+local function UpdateDispelLists(event, ...)
+    local timestamp, type, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, spellId, spellName, destFlag, err, dispel = select(1, ...)
+    local unit = GetLastSpellTarget(dispelSpell)
+    if sourceGUID == UnitGUID("player")
+        and spellId and spellName and spellName == dispelSpell then
+
+        if type:match("^SPELL_CAST") and unit and err and err == "Нечего рассеивать." then
+            for i = 1, 40 do
+                local name, _, _, _, debuffType = UnitDebuff(unit, i,true) 
+                if name and tContains(dispelTypes, debuffType)
+                    and not tContains(DispelWhitelist, name)
+                    and not tContains(DispelBlacklist, name) then
+                    tinsert(DispelBlacklist, name)
+                end
+            end
+        end
+        
+        if type == "SPELL_DISPEL" and not tContains(DispelWhitelist, dispel) then
+            tinsert(DispelWhitelist, dispel)
+        end
+    end
+end    
+
+AttachEvent("COMBAT_LOG_EVENT_UNFILTERED", UpdateDispelLists)
+
+------------------------------------------------------------------------------------------------------------------
+function InForbearance(unit)
+    if unit == nil then unit = "player" end
+    return ((GetTime() - GetSpellLastTime("Гнев карателя") < 30) or HasDebuff("Воздержанность", 0.01, unit))
+end
+------------------------------------------------------------------------------------------------------------------
+
+function DoSpell(spellName, target)
+    if tContains({"Гнев карателя", "Божественный щит", "Возложение рук", "Божественная защита", "Длань защиты"}, spellName) and InForbearance(target) then return false end
+    return UseSpell(spellName, target)
+end
---
+++ sova/rotation.lua
@@ -0,0 +1,110 @@
+﻿-- Sova
+------------------------------------------------------------------------------------------------------------------
+function Idle()
+    if IsAttack() then 
+        if CanExitVehicle() then VehicleExit() return end
+        if IsMounted() then Dismount() return end 
+    end
+    if not IsAttack() and (HasBuff("Пища") or HasBuff("Питье") or IsMounted()) then return end
+        if TryHealing() then return end
+        if TryProtect() then return end
+        if TryBuffs() then return end
+        TryTarget()
+        if HasSpell("Звездопад") then
+            Sova() 
+        else 
+            return
+        end
+end
+
+------------------------------------------------------------------------------------------------------------------
+function Sova()
+    if not HasBuff("Облик лунного совуха") and DoSpell("Облик лунного совуха") then return end
+    if UnitMana100("player") < 50 and DoSpell("Озарение") then return end
+    if not HasDebuff("Волшебный огонь") and DoSpell("Волшебный огонь", target) then return end
+    if not HasDebuff("Земля и луна") and DoSpell("Гнев", target) then end
+    if not HasMyDebuff("Рой насекомых", 0.3) and DoSpell("Рой насекомых", target) then return end
+    if not HasMyDebuff("Лунный огонь", 0.3) and DoSpell("Лунный огонь", target) then return end
+    if HasMyBuff("Лунное затмение") and DoSpell("Звездный огонь", target) then return end
+    if not HasMyBuff("Лунное затмение") and DoSpell("Гнев", target) then return end
+end
+
+------------------------------------------------------------------------------------------------------------------
+function TryBuffs()
+            if not HasBuff("дикой природы") and DoSpell("Знак дикой природы") then return end
+end
+------------------------------------------------------------------------------------------------------------------
+function TryHealing()
+    
+        -- if UnitMana100() < 10 and UseItem("Рунический флакон с зельем маны") then return true end
+
+end
+
+------------------------------------------------------------------------------------------------------------------
+function TryTarget()
+    if not IsValidTarget("target") then
+        local found = false
+        local members = GetGroupUnits()
+        for _,member in pairs(members) do 
+            target = member .. "-target"
+            if not found and IsValidTarget(target) and UnitCanAttack("player", target) and (CheckInteractDistance(target, 2) == 1)  then 
+                found = true 
+                RunMacroText("/startattack " .. target) 
+            end
+        end
+
+        if not (CheckInteractDistance("target", 2) == 1) or not UnitCanAttack("player", "target") then
+            RunMacroText("/cleartarget")
+        end
+
+    end
+
+    if  not IsValidTarget("target") then
+        if GetNextTarget() ~= nil then
+            RunMacroText("/startattack "..GetNextTarget())
+            if not (CheckInteractDistance("target", 2) == 1) or not NextIsTarget() or not UnitCanAttack("player", "target") then
+                RunMacroText("/cleartarget")
+            end
+            ClearNextTarget()
+        end
+    end
+
+    if not IsValidTarget("target") then
+        RunMacroText("/targetenemy [nodead]")
+        
+        if not IsAttack() and not (CheckInteractDistance("target", 2) == 1) or not UnitCanAttack("player", "target") then
+            RunMacroText("/cleartarget")
+        end
+    end
+
+    if not IsValidTarget("target") or (IsAttack() and  not UnitCanAttack("player", "target")) then
+        RunMacroText("/cleartarget")
+    end
+    
+    if not IsValidTarget("focus") then
+        RunMacroText("/clearfocus")
+    end
+    TryEach(TARGETS, function(t)
+        if IsValidTarget(t) and (UnitCreatureType(t) == "Нежить" or UnitCreatureType(t) == "Демон") and IsOneUnit("focus",t) then
+            RunMacroText("/focus " .. t)
+            return true
+        end
+        return false
+    end)
+    
+    
+    if IsArena() and IsValidTarget("target") and (not UnitExists("focus") or IsOneUnit("target", "focus")) then
+        if IsOneUnit("target","arena1") then RunMacroText("/focus arena2") end
+        if IsOneUnit("target","arena2") then RunMacroText("/focus arena1") end
+    end
+    
+    if IsAttack() and not InCombatLockdown() then RunMacroText("/startattack")  end
+end
+
+------------------------------------------------------------------------------------------------------------------
+function TryProtect()
+    if InCombatLockdown() then
+        if (UnitHealth100() < 40) and DoSpell("Дубовая кожа") then return true end
+        end
+    return false
+end
