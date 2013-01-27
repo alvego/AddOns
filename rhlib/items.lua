338ea67af60c6b943afcf285b840d651b553c440
Alvego <alvego@gmail.com>
2013-01-27 21:06:14 +0200

    fix

--- rhlib/items.lua
+++ rhlib/items.lua
@@ -41,13 +41,17 @@ function UseItem(itemName)
     if SpellIsTargeting() then CameraOrSelectOrMoveStart() CameraOrSelectOrMoveStop() end  
     if IsPlayerCasting() then return false end
     if not IsEquippedItem(itemName) and not IsUsableItem(itemName) then return false end
-    local start = GetTime()
-    local try = 0
-    while IsReadyItem(itemName) and GetTime() - start < 1 and try < 3 do
+    local start, try, count, state = GetTime(), 0, IsEquippedItem(itemName) and 3 or 1, IsReadyItem(itemName)
+    while state do
         RunMacroText("/use " .. itemName)
+        if Debug then
+            print(itemName)
+        end
         try = try + 1
+        state = IsReadyItem(itemName) and GetTime() - start < 1 and try < count
     end
-    return true
+    local start, duration = GetItemCooldown(itemName)
+    return start > 0 and (GetTime() - start < 0.01)
 end
 
 ------------------------------------------------------------------------------------------------------------------
