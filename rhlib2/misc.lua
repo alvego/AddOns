bd4986ef326d0805e4d3b8d83f9c8a54bcbb48f8
zz8x <alvego+zz8x@gmail.com>
2013-09-20 21:42:20 +0300

    "Озарение",     "Святая клятва",     "Питье"

--- rhlib2/misc.lua
+++ rhlib2/misc.lua
@@ -65,7 +65,7 @@ notifyFrame:SetScript('OnUpdate', notifyFrame_OnUpdate)
 notifyFrame:Hide()
 notifyFrame.text = notifyFrame:CreateFontString(nil, 'BACKGROUND', 'PVPInfoTextFont')
 notifyFrame.text:SetAllPoints()
-notifyFrame:SetPoint('CENTER', 0, 200)
+notifyFrame:SetPoint('CENTER', 100, 200)
 
 -- Debug messages.
 function Notify(message)
