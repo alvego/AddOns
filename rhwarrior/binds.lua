45a5637e05492fdc4a1f89160ed6e6410dfbc066
zz8x <alvego+zz8x@gmail.com>
2014-01-28 21:51:21 +0200

    push warrior

---
+++ rhwarrior/binds.lua
@@ -0,0 +1,154 @@
+﻿-- Warrior Rotation Helper by Timofeev Alexey
+print("|cff0055ffRotation Helper|r|cffffe00a > |cff804000Warrior|r loaded.")
+-- Binding
+BINDING_HEADER_RHWARRIOR = "Warrior Rotation Helper"
+BINDING_NAME_RHWARRIOR_AOE = "Вкл/Выкл AOE в ротации"
+BINDING_NAME_RHWARRIOR_INTERRUPT = "Вкл/Выкл сбивание кастов"
+------------------------------------------------------------------------------------------------------------------
+if CanAOE == nil then CanAOE = true end
+
+function AOEToggle()
+    CanAOE = not CanAOE
+    if CanAOE then
+        echo("AOE: ON",true)
+    else
+        echo("AOE: OFF",true)
+    end 
+end
+
+function IsAOE()
+   if not CanAOE then return false end
+   if IsShiftKeyDown() == 1 then return true end
+   return (IsValidTarget("target") and InMelee("target") and IsValidTarget("focus") and not IsOneUnit("target", "focus") and InMelee("focus"))
+end
+
+------------------------------------------------------------------------------------------------------------------
+if CanInterrupt == nil then CanInterrupt = true end
+
+function InterruptToggle()
+    CanInterrupt = not CanInterrupt
+    if CanInterrupt then
+        echo("Interrupt: ON",true)
+    else
+        echo("Interrupt: OFF",true)
+    end 
+end
+
+------------------------------------------------------------------------------------------------------------------
+function IsMouse3()
+    return  IsMouseButtonDown(3) == 1
+end
+
+------------------------------------------------------------------------------------------------------------------
+function IsCtr()
+    return  (IsControlKeyDown() == 1 and not GetCurrentKeyBoardFocus())
+end
+
+------------------------------------------------------------------------------------------------------------------
+function IsAlt()
+    return  (IsAltKeyDown() == 1 and not GetCurrentKeyBoardFocus())
+end
+
+------------------------------------------------------------------------------------------------------------------
+
+local nointerruptBuffs = {"Мастер аур"}
+local lichSpells = {"Превращение", "Сглаз", "Соблазн", "Страх", "Вой ужаса", "Контроль над разумом"}
+local conrLichSpells = {"Изгнание зла", "Сковывание нежити"}
+function TryInterrupt(target)
+    if target == nil then target = "target" end
+    if not IsValidTarget(target) then return false end
+    local channel = false
+    local spell, _, _, _, _, endTime, _, _, notinterrupt = UnitCastingInfo(target)
+        
+    if not spell then 
+        spell, _, _, _, _, endTime, _, nointerrupt = UnitChannelInfo(target)
+        channel = true
+    end
+    
+    if not spell then return false end
+
+    if tContains(conrLichSpells, spell) then RunMacroText("/cancelaura Перерождение") end
+
+    if IsPvP() and not InInterruptRedList(spell) then return false end
+    local t = endTime/1000 - GetTime()
+
+    if t < 0.2 then return false end
+    if channel and t < 0.7 then return false end
+
+    m = " -> " .. spell .. " ("..target..")"
+
+    if not notinterrupt and not HasBuff(nointerruptBuffs, 0.1, target) then 
+        
+    end
+   
+
+end
+------------------------------------------------------------------------------------------------------------------
+local lichList = {
+"Сон",
+"Соблазн",
+"Страх", 
+"Вой ужаса", 
+"Устрашающий крик", 
+"Контроль над разумом", 
+"Глубинный ужас", 
+"Ментальный крик"
+}
+
+    
+local exceptionControlList = { -- > 4
+"Ошеломление", -- 20s
+"Покаяние", 
+}
+
+local freedomTime = 0
+function UpdateAutoFreedom(event, ...)
+    if GetTime() - freedomTime < 1.5 then return end
+    local debuff = HasDebuff(lichList, 2, "player")
+    if debuff then 
+        print("lich->freedom", debuff)
+        DoCommand("freedom") 
+        freedomTime = GetTime()
+        return
+    end 
+    debuff = HasDebuff(ControlList, 2, "player")
+    if debuff and (not tContains(exceptionControlList, debuff) or IsAttack()) then 
+        local forceMode = tContains(exceptionControlList, debuff) and IsAttack() and "force!" or ""
+        print("freedom", debuff, forceMode)
+        DoCommand("freedom") 
+        freedomTime = GetTime()
+        return
+    end 
+end
+AttachUpdate(UpdateAutoFreedom, -1)
+------------------------------------------------------------------------------------------------------------------
+
+function DoSpell(spellName, target)
+    return UseSpell(spellName, target)
+end
+------------------------------------------------------------------------------------------------------------------
+
+if TrashList == nil then TrashList = {} end
+function IsTrash(n) --n - itemlink
+    local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(n)
+    if tContains(TrashList, itemName) then return true end
+    --if itemRarity == 2 and (itemType == "Оружие" or itemType == "Доспехи") then return true end
+    return false
+end
+
+
+function trashToggle()
+    local itemName, ItemLink = GameTooltip:GetItem()
+    if nil == itemName then return end
+    if tContains(TrashList, itemName) then 
+        for i=1, #TrashList do
+            if TrashList[i] ==  itemName then 
+                tremove(TrashList, i)
+                chat(itemName .. " это НЕ Хлам! ")
+            end
+        end            
+    else
+        chat(itemName .. " это Хлам! ")
+        tinsert(TrashList, itemName)
+    end
+end
