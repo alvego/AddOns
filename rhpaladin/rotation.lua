1ed153457e5c799c011a5a9fe35ad8e1181e1518
Kluory <kluory@gmail.com>
2018-10-09 21:20:52 +0300

    timer changes

--- rhpaladin/rotation.lua
+++ rhpaladin/rotation.lua
@@ -399,7 +399,7 @@ function PvE()
 
   if not IsInGroup() and not IsOneUnit(player, target .. "-"..target) and DoSpell("Длань возмездия", target) then return end
 
-  if (InMelee(target) or DistanceTo(player, target) < 8) and (IsReadySpell("Божественная буря") or (GetSpellCooldownLeft("Божественная буря") < 0.5)) then
+  if (InMelee(target) or DistanceTo(player, target) < 7) and (IsReadySpell("Божественная буря") or (GetSpellCooldownLeft("Божественная буря") < 0.35)) then
     DoSpell("Божественная буря")
     return
   end
@@ -409,7 +409,7 @@ function PvE()
   if CanMagicAttack(target) and DoSpell(IsAlt() and "Правосудие справедливости" or "Правосудие мудрости", target) then return end
   if HasBuff("Искусство войны") and CanMagicAttack(target) and DoSpell("Экзорцизм", target) then return end
   --if UseEquippedItem("Перчатки ануб'арского охотника") then return end
-  if UseEquippedItem(GetSlotItemName(10), target) then return end
+  if InCombatLockdown() and UseEquippedItem(GetSlotItemName(10), target) then return end
   if (UnitCreatureType(target) == "Нежить") and mana > 30 and DistanceTo(player, target) < 8 and DoSpell("Гнев небес") then return end
   if IsEquippedItemType("Щит") and DoSpell("Щит праведности", target) then return end
   if mana > 30 and InMelee(target) and DoSpell("Освящение") then return end
